2149d528a9470b3b1de13f754d653d4a3a57ebd3
kholilul rk <kholilulrk98@gmail.com>
2020-01-14 02:26:29 +0700

    tugas_1

---
+++ app/Http/Controllers/Store.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Store extends Controller
+{
+
+    public function index()
+    {
+        return view('input');
+    }
+
+    public function save(Request $request)
+    {
+        $nama = $request->input('nama');
+        $tempat = $request->input('tempat');
+        $tanggal = $request->input('tanggal');
+        $jk = $request->input('jk');
+        $email = $request->input('email');
+        $alamat = $request->input('alamat');
+        $data['data']=([
+            'nama'=>$nama,
+            'tempat'=>$tempat,
+            'tanggal'=>$tanggal,
+            'jk'=>$jk,
+            'email'=>$email,
+            'alamat'=>$alamat
+        ]);
+        return view('output', $data);
+    }
+}
+
